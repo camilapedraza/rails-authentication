0e4e8250fcef69443f5d679499f5bfce6716674c
Camila Pedraza <36695574+camilapedraza@users.noreply.github.com>
2023-05-26 12:11:26 +0200

    routes updated for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # user_session, user_password and user_registration controllers are stored in the devise gem!
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
